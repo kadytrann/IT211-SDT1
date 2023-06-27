e19a397f79c098e64ff4d376532c28664d28ef14
Replit user <>
2023-06-27 22:35:38 +0000

    added tell me about yourself assignment contents

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let firstName = Kady;
-let lastName = Tran;
-let yearsOfStudy = 0;
-let goal = land a job in the tech industry;
+let firstName = "Kady";
+let lastName = " Tran";
+let yearsOfStudy = "almost half a year now";
+let goal = "land a job in the tech industry";
