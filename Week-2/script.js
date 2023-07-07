4ca247479a600c8ee7502fc39a288cfa8244bcac
kadytrann <129798540+kadytrann@users.noreply.github.com>
2023-07-06 17:00:42 -0700

    Add files via upload

---
+++ Week-2/script.js
@@ -0,0 +1,11 @@
+/* 
+HW 2 - Pig Latin
+Kady Tran
+
+- If a word begins with a vowel, the letter remains in place and 'way' is appended to the end of the word
+- If a word begins with one consonant, that letter is moved to the end and 'ay' is appended
+- If a word begins with two consonants, both letters are moved to the end and 'ay' is appended
+
+- Use JS strings and conditional logic to write a program that accepts text input and prints out the Pig Latin translation according to the above rules. 
+- If the user enters nothing or a single-letter word, your program should print out a message that says "the input can't be translated."
+*/
